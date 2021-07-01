1b48cbfd1772a2d6cdc1e0b4da25f50db806bb96
Grace Zhang <grxcezhxng@gmail.com>
2021-07-01 15:08:56 -0700

    character count

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -28,6 +28,18 @@
     self.textView.textColor = UIColor.blackColor;
 }
 
+- (void)textViewDidChange:(UITextView *)textView {
+    NSUInteger length;
+      length = [self.textView.text length];
+    self.characterLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)length];
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    const int characterLimit = 140;
+    NSString *newText = [self.textView.text stringByReplacingCharactersInRange:range withString:text];
+    return newText.length < characterLimit;
+}
+
 - (IBAction)handleClose:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
 }
